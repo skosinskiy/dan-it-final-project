21998df90c1d02c9132251b61cc41cd9a7d90b38
Stanislav Kosinskiy <stanislav.kosinski@gmail.com>
2019-02-27 23:28:34 +0200

    added repo token

--- src/main/java/com/danit/finalproject/application/controller/TestConroller.java
+++ src/main/java/com/danit/finalproject/application/controller/TestConroller.java
@@ -4,5 +4,8 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class TestConroller {
-
+	public void testMethod() {
+		System.out.println("");
+		System.out.println("asfasf");
+	}
 }
