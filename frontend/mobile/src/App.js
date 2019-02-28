c8ea7f82cd6e2fb356bf5bc6c7aeb91f54d592fc
Andrii Danilishin <a.i.danilishin@gmail.com>
2019-02-28 23:41:30 +0200

    created empty mobile app

---
+++ frontend/mobile/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Mobile app 
+      </div>
+    );
+  }
+}
+
+export default App;
