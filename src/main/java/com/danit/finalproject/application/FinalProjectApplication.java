8cd5b68aee2b3ed6c359cb7d47dc8cb15897cf6e
Stanislav Kosinskiy <Stanislav.Kosinskiy@alfabank.kiev.ua>
2019-02-28 18:18:31 +0200

    added unused import

--- src/main/java/com/danit/finalproject/application/FinalProjectApplication.java
+++ src/main/java/com/danit/finalproject/application/FinalProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.List;
 import java.util.Map;
 import java.util.LinkedList;
+import java.util.HashMap;
 
 @SpringBootApplication
 public class FinalProjectApplication {
