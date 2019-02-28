59212bd6318008250fc96e4077f53863358f98b3
Stanislav Kosinskiy <stanislav.kosinski@gmail.com>
2019-02-28 22:24:18 +0200

    added unused import

--- src/main/java/com/danit/finalproject/application/controller/TestConroller.java
+++ src/main/java/com/danit/finalproject/application/controller/TestConroller.java
@@ -2,6 +2,7 @@ package com.danit.finalproject.application.controller;
 
 import org.springframework.stereotype.Controller;
 import java.util.List;
+import java.util.HashMap;
 
 @Controller
 public class TestConroller {
