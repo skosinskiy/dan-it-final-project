db984fb6e09374fa360ed6ba6e6244b8d51d7f62
Andrii Danilishin <a.i.danilishin@gmail.com>
2019-03-01 01:50:26 +0200

    created folder structure for mobile app

--- frontend/mobile/src/App.js
+++ frontend/mobile/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
+import Homepage from './pages/HomePage'
 
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Mobile app 
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Switch>
+              <Route exact path='/' component={Homepage} />
+            </Switch>
+        );
+    }
 }
 
 export default App;
