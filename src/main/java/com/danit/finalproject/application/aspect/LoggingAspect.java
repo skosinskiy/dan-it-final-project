5ef5ecc67b99608a1066d5007d66cd47ad338e10
Stanislav Kosinskiy <stanislav.kosinski@gmail.com>
2019-03-02 02:35:17 +0200

    removed unused import

--- src/main/java/com/danit/finalproject/application/aspect/LoggingAspect.java
+++ src/main/java/com/danit/finalproject/application/aspect/LoggingAspect.java
@@ -5,8 +5,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
