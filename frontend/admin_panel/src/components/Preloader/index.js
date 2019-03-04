9ec5cd2f40d2f62224c6bc15e47b5acf9370ed74
DedNikolai <nikolai.blashchuk@gmail.com>
2019-03-04 16:12:12 +0200

    code conversion according to eslint standaarts

--- frontend/admin_panel/src/components/Preloader/index.js
+++ frontend/admin_panel/src/components/Preloader/index.js
@@ -1,22 +1,22 @@
-import React, {Component} from 'react';
-import './preloader.scss';
+import React, {Component} from 'react'
+import './preloader.scss'
 
-export default class Preloader extends Component{
+export default class Preloader extends Component {
     state = {
-        fullscreen: true,
+      fullscreen: true
     }
 
-    render() {
-        const items = [];
-        const fullScreen = this.state.fullscreen ? " full_screen_preloader" : '';
-        for (let i = 0; i < 9; i++) {
-            items.push(<div className={`item-${i+1}`}><div></div></div>)
-        }
+    render () {
+      const items = []
+      const fullScreen = this.state.fullscreen ? ' full_screen_preloader' : ''
+      for (let i = 0; i < 9; i++) {
+        items.push(<div className={`item-${i + 1}`}><div></div></div>)
+      }
 
-        return (
-            <div className={'preloader' + fullScreen}>
-                {items}
-            </div>
-        )
+      return (
+        <div className={'preloader' + fullScreen}>
+          {items}
+        </div>
+      )
     }
 }
