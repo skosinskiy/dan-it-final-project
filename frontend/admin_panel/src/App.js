4be3d31b60bad4ab879675da7ca8369e3246367d
DedNikolai <nikolai.blashchuk@gmail.com>
2019-03-10 17:09:53 +0200

    add page for create business categories

--- frontend/admin_panel/src/App.js
+++ frontend/admin_panel/src/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react'
-import AddBusinessCategory from './components/AddBusinessCategory'
+import React, {Component} from 'react'
 import './App.css'
 
 class App extends Component {
