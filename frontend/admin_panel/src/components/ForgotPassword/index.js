26377a2eac3af463cc991d5dcf240b7d48027a1c
DedNikolai <nikolai.blashchuk@gmail.com>
2019-03-10 14:38:39 +0200

    create forgot password page

--- frontend/admin_panel/src/components/ForgotPassword/index.js
+++ frontend/admin_panel/src/components/ForgotPassword/index.js
@@ -4,15 +4,13 @@ import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import FormControl from '@material-ui/core/FormControl'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Checkbox from '@material-ui/core/Checkbox'
 import Input from '@material-ui/core/Input'
 import InputLabel from '@material-ui/core/InputLabel'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import withStyles from '@material-ui/core/styles/withStyles'
-import { NavLink } from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 const styles = theme => ({
   main: {
