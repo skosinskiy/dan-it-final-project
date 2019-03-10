54fd39e76896a48c4a55d2a82c3f3f083df0c6ba
DedNikolai <nikolai.blashchuk@gmail.com>
2019-03-10 11:08:57 +0200

    add router with secured and unsecured links

--- frontend/screen/src/App.js
+++ frontend/screen/src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
         {/* Learn React */}
         {/* </a> */}
         {/* </header> */}
-        {/* <Preloader /> */}
+        <Preloader />
         <AppRoutes/>
       </div>
     )
