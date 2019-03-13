01bfabcc715526ed9ea6031fcd66e92db62a45f6
Stanislav Kosinskiy <Stanislav.Kosinskiy@alfabank.kiev.ua>
2019-03-13 14:15:55 +0200

    defined another data source for test db

--- src/test/java/com/danit/finalproject/application/controller/UserControllerTest.java
+++ src/test/java/com/danit/finalproject/application/controller/UserControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
@@ -25,24 +24,25 @@ public class UserControllerTest {
 	@Autowired
 	private ObjectMapper objectMapper;
 
+	private static String endpoint = String.format("http://%s:%s/api/users", "localhost", 9000);
+
 	@Test
 	public void getUserById() throws IOException {
-		String endpoint = "http://localhost:8080/api/users/1";
-
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		HttpEntity<Object> requestEntity = new HttpEntity<>(headers);
 
-		ResponseEntity<String> responseEntity = testRestTemplate.exchange(endpoint,
+		ResponseEntity<String> responseEntity = testRestTemplate.exchange(endpoint + "/1",
 				HttpMethod.GET, requestEntity, String.class);
-
 		String responseBody = responseEntity.getBody();
 		User user = objectMapper.readValue(responseBody, User.class);
 
 		Long expectedId = 1L;
+		String expectedEmail = "first.user@test.com";
 
 		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
 		assertEquals(expectedId, user.getId());
+		assertEquals(expectedEmail, user.getEmail());
 
 	}
 
