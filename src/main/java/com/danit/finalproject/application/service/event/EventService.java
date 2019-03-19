604886004fe9ef367c3031d64aea3b4f3a8fd62b
DedNikolai <nikolai.blashchuk@gmail.com>
2019-03-19 12:11:53 +0200

    add endpoints place, business, event

--- src/main/java/com/danit/finalproject/application/service/event/EventService.java
+++ src/main/java/com/danit/finalproject/application/service/event/EventService.java
@@ -10,38 +10,49 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class EventService {
+public
+class EventService {
   private EventRepository eventRepository;
   private BusinessService businessService;
-  private PlaceService placeService;
+  private PlaceService    placeService;
 
   @Autowired
-  public EventService(EventRepository eventRepository,
-    BusinessService businessService,
-    PlaceService placeService) {
+  public
+  EventService(
+      EventRepository eventRepository,
+      BusinessService businessService,
+      PlaceService placeService
+              ) {
     this.eventRepository = eventRepository;
     this.businessService = businessService;
     this.placeService = placeService;
   }
 
-  public Event getEventById(Long id) {
+  public
+  Event getEventById(Long id) {
     return eventRepository.findById(id).orElse(null);
   }
 
-  public List<Event> findAllByLocation(Long placeId, Long businessId) {
-    return eventRepository.findAllByPlaceAndBusiness(placeService.getPlaceById(placeId),
-    businessService.getBusinessById(businessId));
+  public
+  List<Event> findAllByLocation(Long placeId, Long businessId) {
+    return eventRepository.findAllByPlaceAndBusiness(
+        placeService.getPlaceById(placeId),
+        businessService.getBusinessById(businessId)
+                                                    );
   }
 
-  public Event createNewEvent(Event event) {
+  public
+  Event createNewEvent(Event event) {
     return eventRepository.save(event);
   }
 
-  public Event updateEvent(Event event) {
+  public
+  Event updateEvent(Event event) {
     return eventRepository.saveAndFlush(event);
   }
 
-  public void deleteEvent(Long id) {
+  public
+  void deleteEvent(Long id) {
     eventRepository.deleteById(id);
   }
 }
