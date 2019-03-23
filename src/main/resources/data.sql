24525c84f22c51335a13d8b6588b28cb2fcf6124
Stanislav Kosinskiy <stanislav.kosinski@gmail.com>
2019-03-23 18:33:43 +0200

    extended security config

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,9 +20,16 @@ VALUES
 INSERT INTO role_permissions
   (role_id, permission_id)
 VALUES
+  (1, 0),
   (1, 1),
   (1, 2),
-  (1, 3);
+  (1, 3),
+  (2, 0),
+  (2, 1),
+  (2, 2),
+  (2, 3),
+  (2, 4);
+
 
 -- places
 
