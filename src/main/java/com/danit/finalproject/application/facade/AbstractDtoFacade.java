5139072a6ed4705f6a48be7cdab037917e0bedfc
Stanislav Kosinskiy <stanislav.kosinski@gmail.com>
2019-03-25 20:32:03 +0200

    fixed converting null to dto/entity

--- src/main/java/com/danit/finalproject/application/facade/AbstractDtoFacade.java
+++ src/main/java/com/danit/finalproject/application/facade/AbstractDtoFacade.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -47,17 +48,27 @@ public abstract class AbstractDtoFacade<E extends BaseEntity, I, O> {
   }
 
   protected O mapEntityToResponseDto(E entity) {
-    return modelMapper.map(entity, (Class<O>) ((ParameterizedType) getClass()
-        .getGenericSuperclass()).getActualTypeArguments()[2]);
+    if (entity != null) {
+      return modelMapper.map(entity, (Class<O>) ((ParameterizedType) getClass()
+          .getGenericSuperclass()).getActualTypeArguments()[2]);
+    }
+    return null;
   }
 
   protected E mapRequestDtoToEntity(I dto) {
-    return modelMapper.map(dto, (Class<E>) ((ParameterizedType) getClass()
-        .getGenericSuperclass()).getActualTypeArguments()[0]);
+    if (dto != null) {
+      return modelMapper.map(dto, (Class<E>) ((ParameterizedType) getClass()
+          .getGenericSuperclass()).getActualTypeArguments()[0]);
+    }
+    return null;
+
   }
 
   protected List<O> mapEntityListToResponseDtoList(List<E> entityList) {
-    return modelMapper.map(entityList, new TypeToken<List<O>>(){}.getType());
+    if (entityList != null) {
+      return modelMapper.map(entityList, new TypeToken<List<O>>(){}.getType());
+    }
+    return new ArrayList<>();
   }
 
 }
