552df64c1661364119248ac7fb3ddbe2237d8c86
javasia <impex.ua@gmail.com>
2019-03-26 23:52:09 +0200

    submit added

--- frontend/admin_panel/src/components/SidebarMenu/ScreenMenuItems/index.js
+++ frontend/admin_panel/src/components/SidebarMenu/ScreenMenuItems/index.js
@@ -6,6 +6,7 @@ import {fetchAvailableMenuItemNames} from '../../../actions/menuItem'
 import MultipleSelect from './MenuItemMultipleSelect'
 import PlaceCategories from './PlaceCategories'
 import './index.scss'
+import SubmitButton from './SubmitButton'
 
 class ScreenMenuItems extends Component {
   componentDidMount () {
@@ -25,6 +26,7 @@ class ScreenMenuItems extends Component {
       <div className='ScreenMenuWrapper'>
         <PlaceCategories />
         <MultipleSelect isMenuItemNamesLoading={isMenuItemNamesLoading} names={availableMenuItemNames}/>
+        <SubmitButton />
       </div>
     )
   }
