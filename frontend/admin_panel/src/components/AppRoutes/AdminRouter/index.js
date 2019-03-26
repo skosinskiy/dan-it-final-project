1d27077dd94bdf14a424df919c940b88c242bf1d
DedNikolai <nikolai.blashchuk@gmail.com>
2019-03-26 21:54:30 +0200

    restrict access to menu items in admin panel for user depending on it's permissions

--- frontend/admin_panel/src/components/AppRoutes/AdminRouter/index.js
+++ frontend/admin_panel/src/components/AppRoutes/AdminRouter/index.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import AddBusinessCategory from '../../AddBusinessCategory'
 import ManaginrUserRoles from '../../ManagingUserRoles'
 import Places from '../../SidebarMenu/Places'
 import PlaceForm from '../../SidebarMenu/Places/PLaceForm'
+import {hasGrant} from '../../../util/roles'
+import {ProtectedRoute} from '../MainRouter'
+import {Grant} from '../../../util/permissions'
+import {connect} from 'react-redux'
 
 class AppRoutes extends Component {
   render () {
+    const {user} = this.props
+    console.log(user)
     return (
       <Switch>
-        <Route path="/admin/business-categories" component={AddBusinessCategory}/>
-        <Route path="/admin/managing-roles" component={ManaginrUserRoles}/>
-        <Route path="/admin/places/add-new" component={PlaceForm}/>
-        <Route path="/admin/places/:placeId" component={PlaceForm}/>
-        <Route path="/admin/places" component={Places}/>
+        <AuthorizedRoute authorized={hasGrant(user, Grant.MANAGE_BUSINESS_CATEGORIES)} path="/admin/business-categories" component={AddBusinessCategory}/>
+        <AuthorizedRoute authorized={hasGrant(user, Grant.MANGAE_USER_ROLES)} path="/admin/managing-roles" component={ManaginrUserRoles}/>
+        <AuthorizedRoute authorized={hasGrant(user, Grant.MANAGE_PLACES)} path="/admin/places/add-new" component={PlaceForm}/>
+        <AuthorizedRoute authorized={hasGrant(user, Grant.MANAGE_PLACES)} path="/admin/places/:placeId" component={PlaceForm}/>
+        <AuthorizedRoute authorized={hasGrant(user, Grant.MANAGE_PLACES)} path="/admin/places" component={Places}/>
       </Switch>
     )
   }
 }
 
-export default AppRoutes
+export const AuthorizedRoute = ({component: Component, authenticated, authorized, ...rest}) => (
+  <Route {...rest} render={(props) => authorized
+    ? <Route component={Component} {...props}/>
+    : <Redirect to='/login'/>}/>
+)
+
+const mapStateToProps = ({users}) => {
+  return {
+    user: users.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(AppRoutes)
--- frontend/admin_panel/src/components/SidebarMenu/index.js
+++ frontend/admin_panel/src/components/SidebarMenu/index.js
@@ -8,36 +8,49 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 import PeopleIcon from '@material-ui/icons/People'
 import BarChartIcon from '@material-ui/icons/BarChart'
 import LayersIcon from '@material-ui/icons/Layers'
+import {hasGrant} from '../../util/roles'
+import {Grant} from '../../util/permissions'
+import {connect} from 'react-redux'
 import './sidebar-menu.scss'
 
-export default class SidebarMenu extends Component {
+class SidebarMenu extends Component {
   render () {
+    const {user} = this.props
     return (
       <div>
-        <NavLink to={'/admin/business-categories'} className="sidebarItem">
-          <ListItem button>
-            <ListItemIcon>
-              <DashboardIcon />
-            </ListItemIcon>
-            <ListItemText primary={'Business Categories'}/>
-          </ListItem>
-        </NavLink>
-        <NavLink to={'/admin/managing-roles'} className="sidebarItem">
-          <ListItem button>
-            <ListItemIcon>
-              <ShoppingCartIcon />
-            </ListItemIcon>
-            <ListItemText primary={'Managing Roles'}/>
-          </ListItem>
-        </NavLink>
-        <NavLink to={'/admin/places'} className="sidebarItem">
-          <ListItem button>
-            <ListItemIcon>
-              <PeopleIcon />
-            </ListItemIcon>
-            <ListItemText primary="Places" />
-          </ListItem>
-        </NavLink>
+        {
+          hasGrant(user, Grant.MANAGE_BUSINESS_CATEGORIES) &&
+          <NavLink to={'/admin/business-categories'} className="sidebarItem">
+            <ListItem button>
+              <ListItemIcon>
+                <DashboardIcon/>
+              </ListItemIcon>
+              <ListItemText primary={'Business Categories'}/>
+            </ListItem>
+          </NavLink>
+        }
+        {
+          hasGrant(user, Grant.MANGAE_USER_ROLES) &&
+          <NavLink to={'/admin/managing-roles'} className="sidebarItem">
+            <ListItem button>
+              <ListItemIcon>
+                <ShoppingCartIcon/>
+              </ListItemIcon>
+              <ListItemText primary={'Managing Roles'}/>
+            </ListItem>
+          </NavLink>
+        }
+        {
+          hasGrant(user, Grant.MANAGE_PLACES) &&
+          <NavLink to={'/admin/places'} className="sidebarItem">
+            <ListItem button>
+              <ListItemIcon>
+                <PeopleIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Places"/>
+            </ListItem>
+          </NavLink>
+        }
         <ListItem button>
           <ListItemIcon>
             <BarChartIcon />
@@ -54,3 +67,11 @@ export default class SidebarMenu extends Component {
     )
   }
 }
+
+const mapStateToProps = ({users}) => {
+  return {
+    user: users.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(SidebarMenu)
