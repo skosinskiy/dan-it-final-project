471789790972c892c7cf3e5557aef50c1f17d953
DedNikolai <nikolai.blashchuk@gmail.com>
2019-03-27 23:08:02 +0200

    change services

--- src/main/java/com/danit/finalproject/application/repository/ActionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.danit.finalproject.application.repository;
-
-import com.danit.finalproject.application.entity.Action;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActionRepository extends JpaRepository<Action, Long> {}
