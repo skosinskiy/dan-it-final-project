62102ce3d0171118a598b6fbce2d31db3cb7cac4
javasia <impex.ua@gmail.com>
2019-03-28 23:16:09 +0200

    OneToOne import added

--- src/main/java/com/danit/finalproject/application/entity/place/Place.java
+++ src/main/java/com/danit/finalproject/application/entity/place/Place.java
@@ -12,6 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
