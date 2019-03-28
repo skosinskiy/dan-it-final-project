479b8f29dbdabe82099e03982f7019258aee3daa
erom-dm <swiftyerom@gmail.com>
2019-03-29 00:26:29 +0200

    Cleaned up test components, added custom scrollbar styling to App.css

--- frontend/screen/src/App.js
+++ frontend/screen/src/App.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react'
 import {default as Menu} from './components/Menu'
 import './App.css'
 import AppRoutes from './components/AppRoutes'
-import ScrollbarTest from './components/ScrollbarTest'
-import NewsItem from './components/ScrollbarTest/newsItem'
-
-let testArr = [<NewsItem />, <NewsItem />, <NewsItem />, <NewsItem />, <NewsItem />, <NewsItem />, <NewsItem />]
 
 class App extends Component {
   render () {
@@ -13,7 +9,6 @@ class App extends Component {
       <div className='App'>
         <Menu />
         <AppRoutes />
-        <ScrollbarTest newsItems={testArr} />
       </div>
     )
   }
