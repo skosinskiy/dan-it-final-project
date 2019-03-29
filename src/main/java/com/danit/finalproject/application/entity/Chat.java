080cfb3056fa985cb77c0b0a76ae7085c961aa2c
akoziulia <akoziulia@cogniance.com>
2019-03-29 21:58:34 +0200

    Init empty array of messages for chat

--- src/main/java/com/danit/finalproject/application/entity/Chat.java
+++ src/main/java/com/danit/finalproject/application/entity/Chat.java
@@ -12,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -31,5 +32,5 @@ public class Chat extends BaseEntity {
   @OneToMany(mappedBy = "chat")
   @JsonIgnore
   @ToString.Exclude
-  private List<ChatMessage> chatMessages;
+  private List<ChatMessage> chatMessages = new ArrayList<>();
 }
