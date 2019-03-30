77030a89094f354d0a165c8da5d550f4a47f9b0f
Andrii <impex.ua@gmail.com>
2019-03-30 14:37:32 +0200

    repeated imports deleted

--- src/main/java/com/danit/finalproject/application/facade/AbstractDtoFacade.java
+++ src/main/java/com/danit/finalproject/application/facade/AbstractDtoFacade.java
@@ -11,10 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.lang.reflect.ParameterizedType;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
