8f0fdf702e8c8ca2177a72ef9a6854b515bb8be5
Andrii <impex.ua@gmail.com>
2019-04-01 23:32:43 +0300

    fixed up table rendering

--- frontend/admin_panel/src/store/placeCategory/operations.js
+++ frontend/admin_panel/src/store/placeCategory/operations.js
@@ -1,18 +1,30 @@
 import api from 'helpers/FetchData'
 import * as ACTIONS from './actions'
 import {SORTING_ORDER} from 'constants/sortingOrder'
+import DeleteButton from 'pages/admin/components/ManagePlaceCategories/components/Table/components/Buttons/Delete'
+import MultiSelect from 'pages/admin/components/ManagePlaceCategories/components/Table/components/MultiSelect'
+import React from 'react'
 
 export const createData = () => dispatch => {
-  let counter = 0;
-  const process = ({multisync, name, menuItems = "No menu items defined"} = {}) => {
-    counter++
-    return { id: counter, multisync, name, menuItems, delete: "DELETE" }
-  }
+  const mapRawDataToTableRowClosure = mapRawDataToTableRow()
   dispatch(ACTIONS.isLoading(true))
   api.get(`/api/place-categories`)
-    .then(rawData => rawData.map(placeCategory => process(placeCategory)))
+    .then(rawData => rawData.map(placeCategory => mapRawDataToTableRowClosure(placeCategory)))
     .then(placeCategories => dispatch(ACTIONS.createData(placeCategories)))
     .finally(() => dispatch(ACTIONS.isLoading(false)))
+}
+
+const mapRawDataToTableRow = () => {
+  let counter = 0
+  return ({multisync, name, menuItems = "No menu items defined"} = {}) =>
+  ({
+    id: counter++,
+    multisync,
+    name,
+    menuItems: <MultiSelect names={['Lorem', 'Upsum', 'Shops']}/>,
+    delete: <DeleteButton/>,
+    selected: false
+  })
 }
 
 export const updateSelected = placeCategories => dispatch => {
@@ -20,8 +32,8 @@ export const updateSelected = placeCategories => dispatch => {
 }
 
 export const toggleOrder = currentOrder => dispatch => {
-  dispatch(ACTIONS.updateOrder(currentOrder === SORTING_ORDER.ASCENDING &&
-    SORTING_ORDER.DESCENDING || SORTING_ORDER.ASCENDING))
+  dispatch(ACTIONS.updateOrder((currentOrder === SORTING_ORDER.ASCENDING && SORTING_ORDER.DESCENDING) ||
+  SORTING_ORDER.ASCENDING))
 }
 
 export const updateOrderBy = orderBy => dispatch => {
