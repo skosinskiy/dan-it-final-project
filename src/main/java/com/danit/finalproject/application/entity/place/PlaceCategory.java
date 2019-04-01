e53520d8597978ee459fdf9d79f7e1cacbd5b579
Andrii <impex.ua@gmail.com>
2019-04-01 18:32:36 +0300

    added manuitems fields to PlaceCategory entity

--- src/main/java/com/danit/finalproject/application/entity/place/PlaceCategory.java
+++ src/main/java/com/danit/finalproject/application/entity/place/PlaceCategory.java
@@ -1,12 +1,19 @@
 package com.danit.finalproject.application.entity.place;
 
 import com.danit.finalproject.application.entity.BaseEntity;
+import com.danit.finalproject.application.entity.menuitem.MenuItem;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import lombok.ToString;
 
 @Entity
 @Table(name = "places_categories")
@@ -19,4 +26,11 @@ public class PlaceCategory extends BaseEntity {
 
   @Column(name = "multisync")
   private boolean multisync;
+
+  @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+  @JoinTable(name = "placeCategories_menuItems",
+      joinColumns = {@JoinColumn(name = "placeCategory_id")},
+      inverseJoinColumns = {@JoinColumn(name = "menuItem_id")})
+  @ToString.Exclude
+  private List<MenuItem> menuItems;
 }
