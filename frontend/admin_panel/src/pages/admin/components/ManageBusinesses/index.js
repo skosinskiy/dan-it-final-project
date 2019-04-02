9fcf62e7b61ea15f26d90d8b6c2a93b535f25436
erom-dm <swiftyerom@gmail.com>
2019-04-02 21:15:06 +0300

    copied template for BusinessList component

--- frontend/admin_panel/src/pages/admin/components/ManageBusinesses/index.js
+++ frontend/admin_panel/src/pages/admin/components/ManageBusinesses/index.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
 import SearchBar from '../Searchbar'
+import BusinessList from './BusinessList'
 
 class ManagingBusinesses extends Component {
   render () {
     return (
       <div>
         <SearchBar searchtype='business_by_name' />
+        <BusinessList />
       </div>
     )
   }
