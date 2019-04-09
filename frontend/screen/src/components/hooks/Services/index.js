7805a73c8bafa063587781608fc2ea1c12958b57
DedNikolai <nikolai.blashchuk@gmail.com>
2019-04-09 22:25:48 +0300

    add styles

--- frontend/screen/src/components/hooks/Services/index.js
+++ frontend/screen/src/components/hooks/Services/index.js
@@ -1,13 +1,48 @@
 import React, {Component} from 'react'
-import BusinessList from '../../BusinessList'
-import Businesses from '../../../dummy'
+import BusinessItem from '../../BusinessItem'
+import '../../../styles/hooks.scss'
 
-const businessesList = Businesses
+const businesses = [
+  {
+    id: 1,
+    title: 'Service-1',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta\n' +
+    '              praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.',
+    photo: 'https://foodcity.ru/storage/services/August2018/HHEX6ItB8AM42tyUAR5g.jpg',
+    address: 'Address-1'
+  },
+
+  {
+    id: 2,
+    title: 'Service-2',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta\n' +
+    '              praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.',
+    photo: 'https://teplyca.com.ua/wp-content/uploads/2018/04/benef-spa.jpg',
+    address: 'Address-2'
+  },
+
+  {
+    id: 3,
+    title: 'Service-3',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta\n' +
+    '              praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.',
+    photo: 'https://stirka.ua/images/otraslevie_resheniya/kommercheskaya_big.jpg',
+    address: 'Address-3'
+  }
+]
 
 class Services extends Component {
   render () {
+    const businessList = businesses.map((business) => {
+      return <BusinessItem key={business.id} business={business}/>
+    })
     return (
-      <BusinessList/>
+      <>
+      <h1>Services</h1>
+      <div className="businesses-list">
+        {businessList}
+      </div>
+      </>
     )
   }
 }
