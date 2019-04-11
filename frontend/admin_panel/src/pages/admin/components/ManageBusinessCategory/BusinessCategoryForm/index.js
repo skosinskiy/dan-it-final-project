eeb30922d75643c65f0b9c2608e8654d8625bd91
Stanislav Kosinskiy <stanislav.kosinski@gmail.com>
2019-04-11 21:11:43 +0300

    fixed eslint and stylelint

--- frontend/admin_panel/src/pages/admin/components/ManageBusinessCategory/BusinessCategoryForm/index.js
+++ frontend/admin_panel/src/pages/admin/components/ManageBusinessCategory/BusinessCategoryForm/index.js
@@ -221,14 +221,14 @@ class BusinessCategoryForm extends React.Component {
   }
 }
 
-// BusinessCategoryForm.propTypes = {
-//   classes: PropTypes.object.isRequired,
-//   category: PropTypes.object.isRequired,
-//   match:  PropTypes.object.isRequired,
-//   categories: PropTypes.object.isRequired,
-//   getAllBusinessCategories:PropTypes.func.isRequired,
-//   saveCategory: PropTypes.func.isRequired,
-// }
+BusinessCategoryForm.propTypes = {
+  classes: PropTypes.object.isRequired,
+  category: PropTypes.object.isRequired,
+  match:  PropTypes.object.isRequired,
+  categories: PropTypes.object.isRequired,
+  getAllBusinessCategories:PropTypes.func.isRequired,
+  saveCategory: PropTypes.func.isRequired,
+}
 
 const mapStateToProps = (state, props) => {
   const category = state.businessCategory.allBusinessCategories.find(category => category.id === parseInt(props.match.params.categoryId))
