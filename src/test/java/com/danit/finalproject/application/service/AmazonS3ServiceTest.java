0f5955e0cb81a9f6978a85a2ba34e1f3eea5ff85
Stanislav Kosinskiy <stanislav.kosinski@gmail.com>
2019-04-11 21:35:14 +0300

    added test for AmazonS3Service

--- src/test/java/com/danit/finalproject/application/service/AmazonS3ServiceTest.java
+++ src/test/java/com/danit/finalproject/application/service/AmazonS3ServiceTest.java
@@ -1,16 +1,22 @@
 package com.danit.finalproject.application.service;
 
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.danit.finalproject.application.dto.response.S3UploadResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,27 +28,16 @@ public class AmazonS3ServiceTest {
   @MockBean
   private AmazonS3Client amazonS3Client;
 
-//  @Test
-//  public void verifyS3CalledAndKeyIsGenerated() {
-//    int expectedKeyLength = 36;
-//    String objectKey = amazonS3Service.putObject(new File(""));
-//
-//    assertNotNull(objectKey);
-//    assertEquals(expectedKeyLength, objectKey.length());
-//    verify(amazonS3, times(1))
-//        .putObject(eq(AmazonS3Service.S3_BUCKET_NAME), any(), any(File.class));
-//  }
+  @Test
+  public void verifyS3CalledAndKeyIsGenerated() throws IOException {
+    int expectedKeyLength = 40;
+    S3UploadResponse s3UploadResponse = amazonS3Service.putImage(new MockMultipartFile("file", new byte[0]));
 
-//  @Test
-//  public void verifyS3GetCalled() {
-//    when(amazonS3.getObject(eq(AmazonS3Service.S3_BUCKET_NAME), anyString()))
-//        .thenReturn(new S3Object());
-//
-//    amazonS3Service.getObject("");
-//
-//    verify(amazonS3, times(1))
-//        .getObject(eq(AmazonS3Service.S3_BUCKET_NAME), anyString());
-//  }
+    assertNotNull(s3UploadResponse.getFileKey());
+    assertEquals(expectedKeyLength, s3UploadResponse.getFileKey().length());
+    verify(amazonS3Client, times(1))
+        .putObject(eq(AmazonS3Service.S3_BUCKET_NAME), anyString(), any(InputStream.class), eq(null));
+  }
 
   @Test
   public void verifyS3DeleteCalled() {
