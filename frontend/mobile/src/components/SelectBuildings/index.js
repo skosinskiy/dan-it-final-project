9bac95ae4fa10e4960a2e0665990dc7e65eac13b
DedNikolai <nikolai.blashchuk@gmail.com>
2019-04-14 16:13:43 +0300

    create-page-for-selecting-buildings

---
+++ frontend/mobile/src/components/SelectBuildings/index.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react'
+import PlaceItem from './PlaceItem'
+import './select-buildings.scss'
+
+const placesList = [
+  {
+    id: 1,
+    title: 'Home',
+    places: [
+      {
+        id: 1,
+        title: 'Pechersky Lypky',
+        address: 'Lesy Ukrainky, 28'
+      }
+    ]
+  },
+
+  {
+    id: 2,
+    title: 'Office',
+    places: [
+      {
+        id: 2,
+        title: 'Silver Breeze',
+        address: 'Tychyny, 2'
+      }
+    ]
+  },
+
+  {
+    id: 3,
+    title: 'Home',
+    places: [
+      {
+        id: 3,
+        title: 'Sky Mall',
+        address: 'Pochayna, 25'
+      },
+
+      {
+        id: 4,
+        title: 'Forum',
+        address: 'Soborna, 122'
+      }
+    ]
+  }
+]
+
+class SelectBuildings extends Component {
+  render () {
+    const places = placesList.map(place => {
+      return <PlaceItem key={place.id} place={place}/>
+    })
+
+    return (
+      <div className="container">
+        <div className="header">
+          <div className="header-icon">
+          </div>
+        </div>
+        <div className="content">
+          <div className="options">
+            <div className="options-container">
+              <div className="options-title">Explore</div>
+              <div className="options-function">Edit</div>
+            </div>
+          </div>
+          <div className="places">
+            {places}
+          </div>
+          <div className="add-place">
+            <button className="add-place_button">+</button>
+          </div>
+          <div className="footer">
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SelectBuildings
