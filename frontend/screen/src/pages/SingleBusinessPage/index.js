bd99181edadf10100268e0f951a77efd52e41a81
yamnyk <notAunicorN2018>
2019-04-16 17:35:06 +0300

    @yamnyk | render business item to component with no styles

--- frontend/screen/src/pages/SingleBusinessPage/index.js
+++ frontend/screen/src/pages/SingleBusinessPage/index.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {NavLink} from 'react-router-dom'
 import * as businessOperations from "../../store/businesses/operations";
 import './SingleBusinessesPage.scss'
+import Preloader from '../../components/Preloader'
 
 
 class SingleBusinessPage extends Component {
@@ -11,18 +12,31 @@ class SingleBusinessPage extends Component {
 		getAllBusinesses()
 	}
 	render() {
-	  const {businessItem} = this.props
-    console.log(businessItem);
+	  const {businessItems} = this.props
+   
+    if (!businessItems) {
+      return <Preloader/>
+    }
+    const businessItem = businessItems.find((item)=> {
+      return item.id === Number(this.props.match.params.id)
+    });
+    
     return (
-			<div className="business-wrapper">
-				<NavLink to="/businesses/" className="business_back-btn">
+			<div className="bp-wrapper">
+				<NavLink to="/businesses/" className="bp_back-btn">
           Back
 				</NavLink>
-        {/*<p className="bp-title">{businessItem.title}</p>*/}
-        {/*<p className="bp-description">{businessItem.address}</p>*/}
-        {/*<p className="bp-description">{businessItem.description}</p>*/}
+        <p className="bp-title">{businessItem.title}</p>
+        <p className="bp-description">{businessItem.address}</p>
+        <p className="bp-description">{businessItem.description}</p>
+        <p className="bp-description">{businessItem.webSite}</p>
+        <p className="bp-description">{businessItem.phoneNumber}</p>
+        <p className="bp-description">{businessItem.mainPhoto}</p>
         <div className="bp-categories">
-          {/*{categories}*/}
+          {[...businessItem.categories.map(item => <p className="bp-categories-info__text">{item.name}</p>)]}
+        </div>
+        <div className="bp-photos">
+          {[...businessItem.photos.map(item => <p className="bp-photo__item">{item.photo}</p>)]}
         </div>
 			</div>
 		);
@@ -31,10 +45,9 @@ class SingleBusinessPage extends Component {
 
 
 const mapStateToProps = (state, props) => {
-	return {
+  return {
     ...props,
-		// businessList: state.businesses.businessList
-		businessItem: state.businesses.businessList.content[props.match.params.id]
+		businessItems: state.businesses.businessList.content
 	}
 }
 
