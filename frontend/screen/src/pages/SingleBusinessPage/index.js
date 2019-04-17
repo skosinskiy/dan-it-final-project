a359b95edddcfc84338971d0022d1bbb35606824
yamnyk <notAunicorN2018>
2019-04-17 20:07:55 +0300

    @yamnyk | add basic styles

--- frontend/screen/src/pages/SingleBusinessPage/index.js
+++ frontend/screen/src/pages/SingleBusinessPage/index.js
@@ -13,7 +13,7 @@ class SingleBusinessPage extends Component {
 	}
 	render() {
 	  const {businessItem} = this.props
-    debugger
+    // debugger
     if (!businessItem) {
       return <Preloader/>
     }
@@ -23,20 +23,26 @@ class SingleBusinessPage extends Component {
 				<NavLink to="/businesses/" className="bp_back-btn">
           Back
 				</NavLink>
-        <p className="bp-title">{businessItem.title}</p>
-        <p className="bp-address">{businessItem.address}</p>
-        <p className="bp-description">{businessItem.description}</p>
-        <p className="bp-site">{businessItem.webSite}</p>
-        <p className="bp-phone">{businessItem.phoneNumber}</p>
-        <p className="bp-main-photo">{businessItem.mainPhoto}</p>
-        <div className="bp-categories">
-          {[...businessItem.categories.map(item => <p className="bp-categories-info__text">{item.name}</p>)]}
+        <h2 className="bp__title">{businessItem.title}</h2>
+        <div className="bp-info">
+          <img src={businessItem.mainPhoto} alt="business logo" className="bp-info__photo"/>
+          <div className="bp-info_text">
+            <p className="bp-info_text__address">{businessItem.address}</p>
+            <p className="bp-info_text__description">{businessItem.description}</p>
+            <span className="bp-info_text__phone">{businessItem.phoneNumber}</span>
+            <a href={businessItem.webSite} className="bp-info_text__site">{businessItem.webSite}</a>
+            <div className="bp-info__categories">
+              {[...businessItem.categories.map(item => <p key={Math.random()} className="bp-categories-info__text">{item.name}</p>)]}
+            </div>
+          </div>
         </div>
-        <div className="bp-photos">
-          {[...businessItem.photos.map(item => <p className="bp-photo__item">{item.photo}</p>)]}
-        </div>
-        <div className="bp-places">
-          <p className="bp-places__item">{businessItem.place.title}</p>
+        <div className="bp-place-photo-wrapper">
+          <div className="bp-places">
+            <p className="bp-places__item">{businessItem.place.title}</p>
+          </div>
+          <div className="bp-photos">
+            {[...businessItem.photos.map(item => <p key={Math.random()} className="bp-photo__item">{item.photo}</p>)]}
+          </div>
         </div>
 			</div>
 		);
