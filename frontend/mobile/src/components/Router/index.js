d03351bc03618d0657173b44f618b47284cb3fec
DedNikolai <nikolai.blashchuk@gmail.com>
2019-04-17 20:29:07 +0300

    resolve conflicts

---
+++ frontend/mobile/src/components/Router/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import {Route, Switch} from 'react-router-dom'
+
+class Router extends Component {
+  render () {
+    return (
+      <Switch>
+        <Route/>
+      </Switch>
+    )
+  }
+}
