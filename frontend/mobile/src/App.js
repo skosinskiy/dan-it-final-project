79a5e7b16627624548dbb0ff3ba42457a18cd0dd
erom-dm <swiftyerom@gmail.com>
2019-04-18 17:34:47 +0300

    mobile header cleanup, initial contacts page component

--- frontend/mobile/src/App.js
+++ frontend/mobile/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
-// import Preloader from './components/Preloader'
-import NewsPage from './pages/NewsPage'
+// import NewsPage from './pages/NewsPage'
+import ContactsPage from './pages/ContactsPage'
 import './App.css'
 
 class App extends Component {
   render () {
-    // const Content1 = withHeader(anyComponent, image)
     return (
       <div className='App'>
-        <NewsPage />
+        <ContactsPage />
       </div>
     )
   }
---
+++ frontend/mobile/src/pages/ContactsPage/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import './contacts-page.scss'
+
+class ContactsPage extends Component {
+  render () {
+    return (
+      <div className='contactsPage'>
+        Hey
+      </div>
+    )
+  }
+}
+
+export default ContactsPage
