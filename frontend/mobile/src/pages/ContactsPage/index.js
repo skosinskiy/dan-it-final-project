69e43a8afa82db1ef7e35350dadd83acd4da32e8
erom-dm <swiftyerom@gmail.com>
2019-04-18 19:11:53 +0300

    Contact list more styling

--- frontend/mobile/src/pages/ContactsPage/index.js
+++ frontend/mobile/src/pages/ContactsPage/index.js
@@ -1,12 +1,25 @@
 import React, {Component} from 'react'
 import ContactList from './ContactList'
-import './contacts-page.scss'
+import MobileHeader from '../../components/MobileHeader'
 import contactIcon1 from '../../img/ContactsPage/contact-icon1.jpg'
+import headerImage from '../../img/ContactsPage/contactsTemporaryHeaderBg.png'
+import './contacts-page.scss'
 
 class ContactsPage extends Component {
-
   state = {
     contacts: [
+      {name: 'Leto', surname: 'Atreides', location: 'Silver Breeze', image: contactIcon1},
+      {name: 'Leto', surname: 'Atreides', location: 'Silver Breeze', image: contactIcon1},
+      {name: 'Leto', surname: 'Atreides', location: 'Silver Breeze', image: contactIcon1},
+      {name: 'Leto', surname: 'Atreides', location: 'Silver Breeze', image: contactIcon1},
+      {name: 'Leto', surname: 'Atreides', location: 'Silver Breeze', image: contactIcon1},
+      {name: 'Leto', surname: 'Atreides', location: 'Silver Breeze', image: contactIcon1},
+      {name: 'Leto', surname: 'Atreides', location: 'Silver Breeze', image: contactIcon1},
+      {name: 'Leto', surname: 'Atreides', location: 'Silver Breeze', image: contactIcon1},
+      {name: 'Leto', surname: 'Atreides', location: 'Silver Breeze', image: contactIcon1},
+      {name: 'Leto', surname: 'Atreides', location: 'Silver Breeze', image: contactIcon1},
+      {name: 'Leto', surname: 'Atreides', location: 'Silver Breeze', image: contactIcon1},
+      {name: 'Leto', surname: 'Atreides', location: 'Silver Breeze', image: contactIcon1},
       {name: 'Leto', surname: 'Atreides', location: 'Silver Breeze', image: contactIcon1},
       {name: 'Leto', surname: 'Atreides', location: 'Silver Breeze', image: contactIcon1},
       {name: 'Leto', surname: 'Atreides', location: 'Silver Breeze', image: contactIcon1},
@@ -17,9 +30,7 @@ class ContactsPage extends Component {
   render () {
     return (
       <div className='contactsPage parallax-container'>
-        <div>
-          <p>Contacts</p>
-        </div>
+        <MobileHeader bgImage={headerImage} text='Contacts'/>
         <ContactList contacts={this.state.contacts} />
       </div>
     )
