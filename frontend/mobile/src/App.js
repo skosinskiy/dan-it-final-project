89e0de69ce2e9705ccedced6123a841559f70b9b
Dima Kz <@Kuz_13>
2019-04-20 22:26:08 +0300

    fixed-styles

--- frontend/mobile/src/App.js
+++ frontend/mobile/src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react'
 import AppRoutes from './components/AppRoutes'
 import BottomMenu from './components/BottomMenu'
-import Login from './pages/LoginPage'
 import './App.css'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        {/* <AppRoutes />
-        <BottomMenu /> */}
-        <Login />
+        <AppRoutes />
+        <BottomMenu />
       </div>
     )
   }
--- frontend/mobile/src/components/AppRoutes/index.js
+++ frontend/mobile/src/components/AppRoutes/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import NewsPage from '../../pages/NewsPage/index'
-
+import Login from '../../pages/LoginPage/index'
 const AppRoutes = (props) => {
   return (
     <div className={'AppRoutes'}>
       <Switch>
+        <Route path="/login" component={Login}/>
         <Route path="/places" />
         <Route path="/news" component={NewsPage}/>
         <Route path="/messages" />
