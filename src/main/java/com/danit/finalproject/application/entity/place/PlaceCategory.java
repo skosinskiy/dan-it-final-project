a1c41870ca419f3d5dcc8275a36878fdbdf1f0eb
Andrii <impex.ua@gmail.com>
2019-04-21 04:27:01 +0300

    detached entity passed to persist  problem fixed and test-covered

--- src/main/java/com/danit/finalproject/application/entity/place/PlaceCategory.java
+++ src/main/java/com/danit/finalproject/application/entity/place/PlaceCategory.java
@@ -4,17 +4,16 @@ import com.danit.finalproject.application.entity.BaseEntity;
 import com.danit.finalproject.application.entity.business.BusinessCategory;
 import java.util.List;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 @Entity
 @Table(name = "places_categories")
@@ -28,7 +27,7 @@ public class PlaceCategory extends BaseEntity {
   @Column(name = "multisync")
   private boolean multisync;
 
-  @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+  @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
   @JoinTable(name = "placecategories_businesscategories",
       joinColumns = {@JoinColumn(name = "place_category_id")},
       inverseJoinColumns = {@JoinColumn(name = "business_category_id")})
