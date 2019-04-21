6ced0897342c5e07c871ff83e45282f128d365c7
Andrii <impex.ua@gmail.com>
2019-04-21 14:26:13 +0300

    remaining menuitems refs deleted

--- frontend/admin_panel/src/store/menuItems/operations.js
+++
@@ -1,9 +0,0 @@
-import api from 'helpers/FetchData'
-import * as ACTIONS from './actions'
-
-export const fetchNames = () => dispatch => {
-  dispatch(ACTIONS.isLoading(true))
-  api.get(`/api/places/menu-items/`)
-    .then(menuItems => dispatch(ACTIONS.fetchAvailable(menuItems)))
-    .finally(() => dispatch(ACTIONS.isLoading(false)))
-}
--- frontend/admin_panel/src/store/menuItems/types.js
+++
@@ -1,2 +0,0 @@
-export const FETCH_AVAILABLE = 'FETCH_AVAILABLE'
-export const IS_LOADING = 'IS_LOADING'
