5c53dd4cc25b66fe6d7afaa289fd6d5228a7eaa2
ArtemLutsenko <artem.lutsenko@gmail.com>
2019-04-21 12:47:56 +0300

    screen map preloader

--- frontend/screen/src/components/hooks/Map/map.js
+++ frontend/screen/src/components/hooks/Map/map.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './map.scss'
 import Iframe from 'react-iframe'
+import Preloader from '../../Preloader'
 
-const Map = (props) => {
-  return (
-    <div className={'map'}>
-      <Iframe
-        url={'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2541.6655269994703!2d30.59119171589868!3d50.42870309681247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4c57dd85632a3%3A0x60a23243bc1fbf89!2z0KLQpiDQodC40LvRjNCy0LXRgCDQkdGA0LjQtw!5e0!3m2!1sru!2sua!4v1555755290223!5m2!1sru!2sua'}
-        className = "iframe"
-      />
-    </div>
-  )
+class Map extends Component {
+  state = {
+    map: null,
+    error: false,
+    isLoaded: false
+  }
+  componentDidMount() {
+    this.setMap()
+  }
+  setMap = () => {
+    this.setState({
+      map: <Iframe
+          url={'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2541.6655269994703!2d30.59119171589868!3d50.42870309681247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4c57dd85632a3%3A0x60a23243bc1fbf89!2z0KLQpiDQodC40LvRjNCy0LXRgCDQkdGA0LjQtw!5e0!3m2!1sru!2sua!4v1555755290223!5m2!1sru!2sua'}
+          className = "iframe"/>,
+    isLoaded : true
+    })
+  }
+
+  render(){
+    const { map, isLoaded} = this.state;
+    const content = isLoaded ? map : <Preloader />;
+
+    return (
+        <div className={'map'}>
+          {content}
+        </div>
+    )
+  }
 }
 
 export default Map
