b61884e105278d7d80f10be91b98ae084fcc0e12
DedNikolai <nikolai.blashchuk@gmail.com>
2019-04-24 16:53:28 +0300

    show-in-top-menu-items-configured-in-admin-panel

--- frontend/screen/src/components/BusinessList/BusinessItem/index.js
+++ frontend/screen/src/components/BusinessList/BusinessItem/index.js
@@ -4,9 +4,10 @@ import './business-item.scss'
 class BusinessItem extends Component {
   render () {
     const {business} = this.props
+    const img = business.photos.length ? business.photos[0].imageUrl : 'https://foodcity.ru/storage/services/August2018/HHEX6ItB8AM42tyUAR5g.jpg'
     return (
       <div className="business-item">
-        <div className="photo" style={{backgroundImage: `url(${business.photo})`}}></div>
+        <div className="photo" style={{backgroundImage: `url(${img})`}}></div>
         <div className="description">
           <h2>{business.title}</h2>
           <h3>{business.address}</h3>
