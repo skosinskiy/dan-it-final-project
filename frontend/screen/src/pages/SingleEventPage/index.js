c20b656187b39eea373a58b943fe8056fa8a25bb
yamnyk <notAunicorN2018>
2019-04-24 18:52:25 +0300

    @yamnyk | edit preloader case

--- frontend/screen/src/pages/SingleEventPage/index.js
+++ frontend/screen/src/pages/SingleEventPage/index.js
@@ -6,14 +6,14 @@ import Preloader from '../../components/Preloader'
 import './SingleEventPage.scss'
 
 class SingleEventPage extends Component {
-  componentDidMount () {
+  componentWillMount () {
     const {getEventById} = this.props
     getEventById(this.props.match.params.id)
   }
   render () {
     const {eventItem} = this.props
   
-    if (!eventItem) {
+    if (!Object.keys(eventItem).length) {
       return <Preloader/>
     }
     
@@ -24,25 +24,26 @@ class SingleEventPage extends Component {
         </NavLink>
         <h2 className="ep__title">{eventItem.title}</h2>
         <div className="ep-info">
-          {/*<img src={eventItem.mainPhoto} alt="business logo" className="ep-info__photo"/>*/}
+          <img src={eventItem.mainPhoto} alt="business logo" className="ep-info__photo"/>
           <div className="ep-info_text">
-            {/*<p className="ep-info_text__address">{eventItem.address}</p>*/}
+            <p className="ep-info_text__address">{eventItem.address}</p>
             <p className="ep-info_text__description">{eventItem.description}</p>
-            {/*<span className="ep-info_text__phone">{eventItem.phoneNumber}</span>*/}
+            <span className="ep-info_text__date">{eventItem.startDate ? eventItem.startDate : "1.1.11111 18:00"}</span>
+            <span className="ep-info_text__date">{eventItem.endDate ? eventItem.startDate : "1.1.11111 20:00"}</span>
             {/*<a href={eventItem.webSite} className="ep-info_text__site">{eventItem.webSite}</a>*/}
             {/*<div className="ep-info__categories">*/}
               {/*{[...eventItem.categories.map(item => <p key={Math.random()} className="ep-categories-info__text">{item.name}</p>)]}*/}
             {/*</div>*/}
           </div>
         </div>
-        {/*<div className="ep-place-photo-wrapper">*/}
-          {/*<div className="ep-places">*/}
-            {/*<p className="ep-places__item">{eventItem.place.title}</p>*/}
-          {/*</div>*/}
-          {/*<div className="ep-photos">*/}
-            {/*{[...eventItem.photos.map(item => <p key={Math.random()} className="ep-photo__item">{item.photo}</p>)]}*/}
-          {/*</div>*/}
-        {/*</div>*/}
+        <div className="ep-place-photo-wrapper">
+          <div className="ep-places">
+            <p className="ep-places__item">{eventItem.place.title}</p>
+          </div>
+          <div className="ep-photos">
+            {[...eventItem.photos.map(item => <img key={Math.random()} className="ep-photo__item" src={item.imageUrl} alt="event-photos-item"/>)]}
+          </div>
+        </div>
       </div>
     )
   }
