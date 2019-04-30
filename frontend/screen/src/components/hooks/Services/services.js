d66e68cf337c8456178e40cf89efea366c5f4c45
yamnyk <notAunicorN2018>
2019-04-30 14:02:46 +0300

    @yamnyk | add scroll event listener

--- frontend/screen/src/components/hooks/Services/services.js
+++ frontend/screen/src/components/hooks/Services/services.js
@@ -29,20 +29,84 @@ const businesses = [
     '              praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.',
     photo: 'https://stirka.ua/images/otraslevie_resheniya/kommercheskaya_big.jpg',
     address: 'Address-3'
+  },
+
+  {
+    id: 4,
+    title: 'Service-4',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta\n' +
+    '              praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.',
+    photo: 'https://stirka.ua/images/otraslevie_resheniya/kommercheskaya_big.jpg',
+    address: 'Address-3'
+  },
+
+  {
+    id: 5,
+    title: 'Service-5',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta\n' +
+    '              praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.',
+    photo: 'https://stirka.ua/images/otraslevie_resheniya/kommercheskaya_big.jpg',
+    address: 'Address-3'
+  },
+
+  {
+    id: 6,
+    title: 'Service-6',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta\n' +
+    '              praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.',
+    photo: 'https://stirka.ua/images/otraslevie_resheniya/kommercheskaya_big.jpg',
+    address: 'Address-3'
+  },
+
+  {
+    id: 7,
+    title: 'Service-7',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta\n' +
+    '              praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.',
+    photo: 'https://stirka.ua/images/otraslevie_resheniya/kommercheskaya_big.jpg',
+    address: 'Address-3'
   }
 ]
 
 class Services extends Component {
-  render () {
-    const {businessesByCategory} = this.props
-    console.log(businessesByCategory)
-    const businessList = businesses.map((business) => {
+  componentDidMount() {
+    // Detect when scrolled to bottom.
+    this.refs.myscroll.addEventListener("scroll", () => {
+      const scrollTo = this.refs.myscroll.clientHeight * 0.9
+      const scrollTop = this.refs.myscroll.scrollTop
+      // if (
+      //   this.refs.myscroll.scrollTop + this.refs.myscroll.clientHeight >=
+      //   this.refs.myscroll.scrollHeight
+      // ) {
+      //   this.loadMore();
+      // }
+      if(scrollTop >= scrollTo) {
+        this.loadMore();
+      }
+    });
+  }
+  
+  loadFirstScreen = (itemsNumber) => {
+    const elemsCount = itemsNumber - 1;
+    return businesses.filter((item, index) => index <= elemsCount).map((business) => {
       return <BusinessItem key={business.id} business={business}/>
     })
+  };
+  
+  loadMore () {
+    console.log('fetching data')
+  }
+  
+  render () {
+    // const {businessesByCategory} = this.props
+    // console.log(businessesByCategory)
+    const businessList = this.loadFirstScreen(4);
     return (
       <>
         <h1>Services</h1>
-        <div className="businesses-list">
+        <div
+          ref='myscroll'
+          className="businesses-list">
           {businessList}
         </div>
       </>
