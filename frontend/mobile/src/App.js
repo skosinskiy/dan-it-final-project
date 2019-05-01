6412a08bd01000b3b71369298a7ea870f734698a
Stanislav Kosinskiy <stanislav.kosinski@gmail.com>
2019-05-02 01:19:32 +0300

    fixed eslint

--- frontend/mobile/src/App.js
+++ frontend/mobile/src/App.js
@@ -6,8 +6,7 @@ import {connect} from 'react-redux'
 import * as usersOperations from './store/users/operations'
 
 class App extends Component {
-
-  componentDidMount() {
+  componentDidMount () {
     this.props.getCurrentUser()
   }
 
@@ -32,8 +31,8 @@ const mapDispatchToProps = dispatch => ({
 })
 
 export default withRouter(
-    connect(
-        mapStateToProps,
-        mapDispatchToProps
-    )(App)
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(App)
 )
