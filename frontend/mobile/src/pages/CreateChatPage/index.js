dd5de14dc2c5d5b9f2be3fe56a4e946f73a771f3
erom-dm <swiftyerom@gmail.com>
2019-05-02 16:33:51 +0300

    setup

---
+++ frontend/mobile/src/pages/CreateChatPage/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CreateChatPage extends Component {
+  render () {
+    return (
+      <div className="create-chat">
+
+      </div>
+    )
+  }
+}
+
+export default CreateChatPage
