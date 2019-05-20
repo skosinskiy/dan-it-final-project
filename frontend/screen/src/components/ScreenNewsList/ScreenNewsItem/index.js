3f417e92561ef9e28904636408c230351d8b0c9d
erom-dm <swiftyerom@gmail.com>
2019-05-20 17:54:22 +0300

    Basic RSS implementation

--- frontend/screen/src/components/ScreenNewsList/ScreenNewsItem/index.js
+++ frontend/screen/src/components/ScreenNewsList/ScreenNewsItem/index.js
@@ -3,11 +3,10 @@ import './ScreenNewsItem.scss'
 
 const ScreenEventItem = (props) => {
   return (
-    <div className={'screenNewsItem'} onClick={() => (console.log("window.location='http://google.com'"))}>
-      <div className={'screenNewsItem-img'}/>
-      { /* <img className={'screenNewsItem-img'} src={props.img} alt="news-item img" /> */}
+    <a className={'screenNewsItem'} href={props.link}>
+      <img className={'screenNewsItem-img'} src={props.img} alt="news-item img" />
       <p className={'screenNewsItem-description'}>{props.description}</p>
-    </div>
+    </a>
   )
 }
 
