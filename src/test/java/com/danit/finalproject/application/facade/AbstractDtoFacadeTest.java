3076f5a2db53c7fc1cf15f03361b63338e5daa4c
Stanislav Kosinskiy <stanislav.kosinski@gmail.com>
2019-05-26 16:58:45 +0300

    added tests

--- src/test/java/com/danit/finalproject/application/facade/AbstractDtoFacadeTest.java
+++ src/test/java/com/danit/finalproject/application/facade/AbstractDtoFacadeTest.java
@@ -1,8 +1,7 @@
 package com.danit.finalproject.application.facade;
 
-import com.danit.finalproject.application.dto.request.business.BusinessRequest;
-import com.danit.finalproject.application.dto.response.business.BusinessResponse;
 import com.danit.finalproject.application.entity.business.Business;
+import com.danit.finalproject.application.facade.business.BusinessFacade;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,27 +19,27 @@ import static org.junit.Assert.assertTrue;
 public class AbstractDtoFacadeTest {
 
   @Autowired
-  private AbstractDtoFacade<Business, BusinessRequest, BusinessResponse> abstractDtoFacade;
+  private BusinessFacade businessFacade;
 
   @Test
   public void mapNullEntityToResponseDtoTest() {
-    assertNull(abstractDtoFacade.mapEntityToResponseDto(null));
+    assertNull(businessFacade.mapEntityToResponseDto(null));
   }
 
   @Test
   public void mapNullRequestDtoToEntity() {
-    assertNull(abstractDtoFacade.mapRequestDtoToEntity(null));
+    assertNull(businessFacade.mapRequestDtoToEntity(null));
   }
 
   @Test
   public void mapNullEntityListToResponseDtoList() {
     List <Business> businesses = null;
-    assertTrue(abstractDtoFacade.mapEntityListToResponseDtoList(businesses).isEmpty());
+    assertTrue(businessFacade.mapEntityListToResponseDtoList(businesses).isEmpty());
   }
 
   @Test
   public void mapNullEntityListToResponseDtoPage() {
     Page<Business> businesses = null;
-    assertNull(abstractDtoFacade.mapEntityListToResponseDtoList(businesses));
+    assertNull(businessFacade.mapEntityListToResponseDtoList(businesses));
   }
 }
