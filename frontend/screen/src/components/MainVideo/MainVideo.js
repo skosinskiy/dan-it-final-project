dfce5af90bc13068005e2168060d313efdf4b332
ArtemLutsenko <artem.lutsenko@gmail.com>
2019-06-08 07:55:30 +0300

    Screen video

--- frontend/screen/src/components/MainVideo/MainVideo.js
+++ frontend/screen/src/components/MainVideo/MainVideo.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import './MainVideo.scss'
 
+
 const MainVideo = () => {
-  return (
-    <div className={'MainVideo'}>
-      <img className={'MainVideo-gif'} src={require('../../img/DummyImg/nature.gif')} alt ='img' />
-    </div>
+  return(
+      <div className={'MainVideo'}>
+        <iframe className={'MainVideo__iframe'}  src="https://www.youtube.com/embed/dqQEX2ZMeoE" frameBorder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen></iframe>
+      </div>
   )
 }
+// const MainVideo = () => {
+//   return (
+//     <div className={'MainVideo'}>
+//       <img className={'MainVideo-gif'} src={require('../../img/DummyImg/nature.gif')} alt ='img' />
+//     </div>
+//   )
+// }
 
 export default MainVideo
